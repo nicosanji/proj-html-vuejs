62f535a7e05130de5ac5586d9f34d7fe5c9e5777
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-11-22 16:58:20 +0100

    Creato componente Header completo

--- src/App.vue
+++ src/App.vue
@@ -18,4 +18,61 @@ export default {
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
 @import "~font-awesome/css/font-awesome.min.css";
+
+header {
+  img {
+    width: 5%;
+    height: 5%;
+    margin-right: auto;
+  }
+  ul {
+    list-style: none;
+    padding-left: 0;
+    margin-bottom: 0;
+    li {
+      display: inline;
+      a {
+        text-decoration: none;
+        color: black;
+        font-weight: 500;
+        &:hover {
+          color: #377dff;
+        }
+      }
+    }
+    .active {
+      color: #377dff;
+      padding-bottom: 15px;
+      border-bottom: 2px solid #377dff;
+    }
+  }
+  p {
+    margin-bottom: 0;
+    cursor: pointer;
+    &:hover {
+      color: #377dff;
+    }
+    span {
+      background-color: #377dff;
+      color: white;
+      font-weight: 500;
+      right: 6px;
+      bottom: 8px;
+      height: 18px;
+      width: 18px;
+      font-size: 10px;
+      line-height: 18px;
+      text-align: center;
+      border-radius: 50%;
+      z-index: -1;
+    }
+  }
+  .btn {
+    background-color: #377dff;
+    &:hover {
+      background-color: black;
+      border-color: black;
+    }
+  }
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,61 @@
 <template>
-  <div class="container">
-    <h1>Header con bootstrap</h1>
-    <h1><i class="fa fa-bomb" aria-hidden="true"></i></h1>
-  </div>
+  <header>
+    <div class="container d-flex align-items-center">
+      <img src="../assets/img/logo-2.png" alt="logo-2" class="py-2" />
+      <ul>
+        <li v-for="(element, i) in headerLinks" :key="i" class="px-3">
+          <a
+            @click.prevent="currentIndex = i"
+            :class="i === currentIndex ? 'active' : ''"
+            href="element.href"
+            >{{ element.txt }}</a
+          >
+        </li>
+      </ul>
+      <p class="ps-5 pe-3"><i class="fa fa-search" aria-hidden="true"></i></p>
+      <p class="px-3 position-relative">
+        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+        <span class="position-absolute">0</span>
+      </p>
+      <div>
+        <button type="button" class="btn text-white px-3 py-2 ms-5">
+          Get Started
+        </button>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      currentIndex: 0,
+      headerLinks: [
+        {
+          txt: "Home",
+          href: "home",
+        },
+        {
+          txt: "About us",
+          href: "about-us",
+        },
+        {
+          txt: "Feature",
+          href: "feature",
+        },
+        {
+          txt: "Testimonials",
+          href: "testimonials",
+        },
+        {
+          txt: "Contact US",
+          href: "contact-us",
+        },
+      ],
+    };
+  },
 };
 </script>
 
